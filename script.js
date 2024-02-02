9a36698fa57c9832d8f2c9cec43fbc047a041ef7
WerleAna <anacarolinewerle15@gmail.com>
2024-02-01 21:45:09 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function toggleMode(){
+   const html= document.documentElement
+
+  //  if(html.classList.contains('light')){
+  //      html.classList.remove('light')
+  //  } else {
+  //      html.classList.add('light')
+  //  }
+
+  // outro jeito de fazer 
+
+  const img = document.querySelector("#profile img")
+
+  html.classList.toggle('light')
+
+  if(html.classList.contains('light')){
+    img.setAttribute('src','./assets/avatar.png')
+  } else {
+    img.setAttribute('src', './assets/avatar-dark.png' )
+  }
+
+
+}
