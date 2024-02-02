9e2d878e35c2264ac7f250037e3999b9677612f5
WerleAna <anacarolinewerle15@gmail.com>
2024-02-01 21:53:15 -0300

    Initial commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function toggleMode(){
    const html= document.documentElement
+   html.classList.toggle('light')
 
   //  if(html.classList.contains('light')){
   //      html.classList.remove('light')
@@ -10,8 +11,6 @@ function toggleMode(){
   // outro jeito de fazer 
 
   const img = document.querySelector("#profile img")
-
-  html.classList.toggle('light')
 
   if(html.classList.contains('light')){
     img.setAttribute('src','./assets/avatar.png')
